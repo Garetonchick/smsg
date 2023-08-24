e9d55184c8180f830427e3134b03be9cca71ebc0
Garetonchick <dsbonich@edu.hse.ru>
2023-08-24 19:07:37 +0300

    Add json parser and config

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <string_view>
+#include <fstream>
+#include <exception>
+
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib/httplib.h>
+#include <jsoncpp/json/json.h>
+
+Json::Value LoadJson(const char *file_name) {
+    Json::Value val;
+    std::ifstream ifs(file_name);
+
+    if (!ifs.is_open()) {
+        throw std::runtime_error("Failed to open json file");
+    }
+
+    Json::CharReaderBuilder builder;
+    std::string errs;
+    if (!Json::parseFromStream(builder, ifs, &val, &errs)) {
+        throw std::runtime_error("Failed to load/parse json");
+    }
+    return val;
+}
 
 int main() {
-    httplib::SSLServer svr("./ssl/cert.pem", "./ssl/key.pem");
+    Json::Value conf = LoadJson("./config.json");
+    httplib::SSLServer svr(conf["ssl_cert_path"].asCString(), conf["ssl_key_path"].asCString());
 
     svr.Get("/hey", [](const httplib::Request &, httplib::Response &res) {
         res.set_content("Hello World!", "text/plain");
     });
 
-    svr.listen("0.0.0.0", 8080);
+    svr.listen(conf["server_ip"].asString(), conf["server_port"].asInt());
 }
