09cedfb9066e4ac133720846f7efeac3144e1b96
Garetonchick <dsbonich@edu.hse.ru>
2023-08-24 21:03:37 +0300

    Add serving of static files

--- src/main.cpp
+++ src/main.cpp
@@ -27,8 +27,10 @@ int main() {
     Json::Value conf = LoadJson("./config.json");
     httplib::SSLServer svr(conf["ssl_cert_path"].asCString(), conf["ssl_key_path"].asCString());
 
+    svr.set_mount_point("/static", conf["static_path"].asCString());
+
     svr.Get("/hey", [](const httplib::Request &, httplib::Response &res) {
-        res.set_content("Hello World!", "text/plain");
+        res.set_content("<html><body><h1>Hey smsg!</h1></body></html>", "text/html");
     });
 
     svr.listen(conf["server_ip"].asString(), conf["server_port"].asInt());
