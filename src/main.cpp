d0f1732655379d9645a49911289db53f12256bf9
Garetonchick <dsbonich@edu.hse.ru>
2023-08-24 17:36:04 +0300

    Add ssl

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <thread>
-// #define CPPHTTPLIB_OPENSSL_SUPPORT
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib/httplib.h>
 
 int main() {
-    httplib::Server svr;
+    httplib::SSLServer svr("./ssl/cert.pem", "./ssl/key.pem");
 
     svr.Get("/hey", [](const httplib::Request &, httplib::Response &res) {
         res.set_content("Hello World!", "text/plain");
