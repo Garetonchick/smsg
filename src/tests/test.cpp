811a47e8c7aa0b52422c7760d0669c187db3984b
Garetonchick <dsbonich@edu.hse.ru>
2023-12-21 23:33:52 +0300

    Add libpqxx

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <fstream>
+#include <pqxx/pqxx>
 #include "../util.h"
 
 TEST(MyTestSuite, MyTestCase) {
@@ -24,6 +25,55 @@ TEST(MyTestSuite, MyTestCase) {
     ASSERT_EQ(test4["age"].asInt(), 30);
     ASSERT_STREQ(test4["address"]["street"].asCString(), "123 Main St");
 
+}
+
+TEST(MyTestSuite, DBTest){
+    try {
+        pqxx::connection c;
+        std::cout << "Connected to " << c.dbname() << '\n';
+
+        pqxx::work tx{c};
+
+        for (auto [name, salary] : tx.query<std::string, int>(
+            "SELECT name, salary FROM employee ORDER BY name"))
+        {
+            std::cout << name << " earns " << salary << ".\n";
+        }
+
+        for (auto [name, salary] : tx.stream<std::string_view, int>(
+            "SELECT name, salary FROM employee"))
+        {
+            std::cout << name << " earns " << salary << ".\n";
+        }
+
+        std::cout << "Doubling all employees' salaries...\n";
+        tx.exec0("UPDATE employee SET salary = salary*2");
+
+        int my_salary = tx.query_value<int>(
+            "SELECT salary FROM employee WHERE name = 'Me'");
+        std::cout << "I now earn " << my_salary << ".\n";
+
+        auto [top_name, top_salary] = tx.query1<std::string, int>(
+            R"(
+                SELECT name, salary
+                FROM employee
+                WHERE salary = max(salary)
+                LIMIT 1
+            )");
+        std::cout << "Top earner is " << top_name << " with a salary of "
+                  << top_salary << ".\n";
+
+        pqxx::result res = tx.exec("SELECT * FROM employee");
+        std::cout << "Columns:\n";
+        for (pqxx::row_size_type col = 0; col < res.columns(); ++col)
+            std::cout << res.column_name(col) << '\n';
+
+        std::cout << "Making changes definite: ";
+        tx.commit();
+        std::cout << "OK.\n";
+    } catch(...) {
+        // ok
+    }
 }
 
 int main(int argc, char **argv) {
